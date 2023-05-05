cdbff4060c4b399ab35df7a45bbb5efac7d472d9
stefan <superstefo@gmail.com>
2023-05-05 22:41:08 +0300

    test

--- src/app/testings/page.js
+++ src/app/testings/page.js
@@ -238,7 +238,7 @@ export default function Page() {
               Password
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-            <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+            <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you would like</p>
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-2">
