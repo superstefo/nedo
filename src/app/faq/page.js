a249db465e43391e1aa21c0a08f6530c1caf7557
stefan <superstefo@gmail.com>
2023-05-16 22:51:05 +0300

    faq

---
+++ src/app/faq/page.js
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 max-w-5xl justify-center">
+
+      <p className="">
+        <div className="font-mono font-bold"> Възмжно ли е изследването за инсулин да покаже фалшиви ниски резултати?
+        </div>
+        Да, ако има наличие на влошено здраве на червените кръвни телца.
+      </p>
+      <br></br>
+      <p className="">
+        <div className="font-mono font-bold"> Кои витамини подобряват Инсулиновата чувствителност?
+        </div>
+        Всички витамини са важни, но витамин Д доказано подобрява Инсулиновата чувствителност. Дори има твърдения, че пречи на развитието на диабет тип 2.
+      </p>
+      <br></br>
+      <p className="">
+        <div className="font-mono font-bold"> bbbbbb Владиславче, какво правиш?
+        </div>
+        bbbbbbbbbbbbbb ddd Владиславче, какво правиш??? Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???
+      </p>
+      <br></br>
+      <p className="">
+        <div className="font-mono font-bold"> bbbbbb Владиславче, какво правиш?
+        </div>
+        bbbbbbbbbbbbbb ddd Владиславче, какво правиш??? Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???Владиславче, какво правиш???
+      </p>
+
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Item = ({ title, href, info }) => {
   return (
-    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30">
+    <div className="group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30">
       <Link href={href}>  <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
         {title}{' '}
         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -15,27 +14,35 @@ const Item = ({ title, href, info }) => {
       </h2>
       </Link>
       <p className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}>
-         {info}
-        </p>
+        {info}
+      </p>
     </div>
   );
 }
 
 export default function Home() {
-
-  return ( <div>
-
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-1 lg:text-left"> <span> koooe Този сайт има за цел да подпомогне лечението на пациенти с инсулинова резистентност и други подобни метаболитни нарушения. 
-Сайтът е авторски </span> </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-
-     <Item href= "/ir" title= "Съдържание" info="какво има в тази страница"/>
-     <Item href= "/ir" title= "Питанки" info="въпроси и отговори"/>
-     <Item href= "/ir" title= "Анализ" info="анализ на изследвания с изкуствен интелект"/>
-     <Item href= "/ir" title= "Контакти" info="свържете се с нас"/>
-      </div>
-    </main>
-     </div>
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-1 lg:text-center gap-6 max-w-5xl">
+          <br></br>
+          <span > koooe Този сайт има за цел да подпомогне лечението на пациенти с инсулинова резистентност и други подобни метаболитни нарушения.
+            Сайтът е авторски koooe Този сайт има за цел да подпомогне лечението на пациенти с инсулинова резистентност и други подобни метаболитни нарушения.
+            Сайтът е авторски koooe
+          </span>
+        </div>
+        <br></br>
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-center">
+          <Item href="/ir" title="Теория" info="информация, статии, материали" />
+          <Item href="/ir" title="Симптоми" info="въпроси и отговори" />
+          <Item href="/faq" title="Питанки" info="въпроси и отговори" />
+          <Item href="/ir" title="Дневник" info="хранителен дневник" />
+          <Item href="/ir" title="Анализ" info="анализ на изследвания с изкуствен интелект" />
+          <Item href="/ir" title="Лекарства" info="мнения и препоръки" />
+          <Item href="/ir" title="Лекари" info="мнения и препоръки" />
+          <Item href="/ir" title="Контакти" info="свържете се с нас" />
+        </div>
+      </main>
+    </div>
   )
 }
